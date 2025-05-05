9af3e4d86f260902895ed967fb0ff01ae15e9b1a
Toothless <hyunjinney@naver.com>
2025-05-05 21:10:10 +0900

    ETC
    - Network Manager UI
    - Player Type Array
    - Test Winner Function
    - GameOver UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,40 +1,112 @@
 using System;
+using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class GameManager : NetworkBehaviour
 {
-    public static GameManager Instance{get; private set;}
+    public static GameManager Instance { get; private set; }
 
     public event EventHandler<OnClickedOnGridPositionEventArgs> OnClickedOnGridPosition;
-    public class OnClickedOnGridPositionEventArgs : EventArgs{
+    public class OnClickedOnGridPositionEventArgs : EventArgs
+    {
         public int x;
         public int y;
         public PlayerType playerType;
     }
 
     public event EventHandler OnGameStarted;
+    public event EventHandler<OnGameWinEventArgs> OnGameWin;
+    public class OnGameWinEventArgs : EventArgs
+    {
+        public Line line;
+        public PlayerType winPlayerType;
+    }
     public event EventHandler OnCurrentPlayablePlayerTypeChanged;
 
-    public enum PlayerType{
+    public enum PlayerType
+    {
         None,
         Cross,
         Circle
     }
+    public enum Orientation
+    {
+        Horizontal,
+        Vertical,
+        DiagonalA,
+        DiagonalB
+    }
+    public struct Line 
+    {
+        public List<Vector2Int> gridVector2IntList;
+        public Vector2Int centerGridPosition;
+        public Orientation orientation;
+
+    }
     private PlayerType localPlayerType;
     private NetworkVariable<PlayerType> currentPlayablePlayerType = new NetworkVariable<PlayerType>();
+    private PlayerType[,] playerTypeArray;
+    private List<Line> lineList;
+
     private void Awake()
     {
-        if(Instance !=null)
+        if (Instance != null)
         {
             Debug.LogError("More than one Gamemanager Instance");
         }
         Instance = this;
+        playerTypeArray = new PlayerType[3, 3];
+        lineList = new List<Line>{
+            //Horizontal
+            new Line{
+                gridVector2IntList = new List<Vector2Int>{new Vector2Int(0,0), new Vector2Int(1,0), new Vector2Int(2,0),},
+                centerGridPosition = new Vector2Int(1,0),
+                orientation = Orientation.Horizontal
+            },
+            new Line{
+                gridVector2IntList = new List<Vector2Int>{new Vector2Int(0,1), new Vector2Int(1,1), new Vector2Int(2,1),},
+                centerGridPosition = new Vector2Int(1,1),
+                orientation = Orientation.Horizontal
+            },
+            new Line{
+                gridVector2IntList = new List<Vector2Int>{new Vector2Int(0,2), new Vector2Int(1,2), new Vector2Int(2,2),},
+                centerGridPosition = new Vector2Int(1,2),
+                orientation = Orientation.Horizontal
+            },
+            //Vetical
+            new Line{
+                gridVector2IntList = new List<Vector2Int>{new Vector2Int(0,0), new Vector2Int(0,1), new Vector2Int(0,2),},
+                centerGridPosition = new Vector2Int(0,1),
+                orientation = Orientation.Vertical
+            },
+            new Line{
+                gridVector2IntList = new List<Vector2Int>{new Vector2Int(1,0), new Vector2Int(1,1), new Vector2Int(1,2),},
+                centerGridPosition = new Vector2Int(1,1),
+                orientation = Orientation.Vertical
+            },
+            new Line{
+                gridVector2IntList = new List<Vector2Int>{new Vector2Int(2,0), new Vector2Int(2,1), new Vector2Int(2,2),},
+                centerGridPosition = new Vector2Int(2,1),
+                orientation = Orientation.Vertical
+            },
+            //Diagonals
+            new Line{
+                gridVector2IntList = new List<Vector2Int>{new Vector2Int(0,0), new Vector2Int(1,1), new Vector2Int(2,2),},
+                centerGridPosition = new Vector2Int(1,1),
+                orientation = Orientation.DiagonalA
+            },
+            new Line{
+                gridVector2IntList = new List<Vector2Int>{new Vector2Int(0,2), new Vector2Int(1,1), new Vector2Int(2,0),},
+                centerGridPosition = new Vector2Int(1,1),
+                orientation = Orientation.DiagonalB
+            }
+        };
     }
     public override void OnNetworkSpawn()
     {
-        if(NetworkManager.Singleton.LocalClientId == 0)
+        if (NetworkManager.Singleton.LocalClientId == 0)
         {
             localPlayerType = PlayerType.Cross;
         }
@@ -43,18 +115,19 @@ public class GameManager : NetworkBehaviour
             localPlayerType = PlayerType.Circle;
         }
 
-        if(IsServer)
+        if (IsServer)
         {
             NetworkManager.Singleton.OnClientConnectedCallback += NetworkManager_OnClientConnectedCallBack;
         }
-        currentPlayablePlayerType.OnValueChanged += (PlayerType oldPlayerType, PlayerType newPlayerType) => {
+        currentPlayablePlayerType.OnValueChanged += (PlayerType oldPlayerType, PlayerType newPlayerType) =>
+        {
             OnCurrentPlayablePlayerTypeChanged?.Invoke(this, EventArgs.Empty);
         };
     }
 
     private void NetworkManager_OnClientConnectedCallBack(ulong obj)
     {
-        if(NetworkManager.Singleton.ConnectedClientsList.Count == 2)
+        if (NetworkManager.Singleton.ConnectedClientsList.Count == 2)
         {
             //Start Game
             currentPlayablePlayerType.Value = PlayerType.Cross;
@@ -65,23 +138,31 @@ public class GameManager : NetworkBehaviour
     [Rpc(SendTo.ClientsAndHost)]
     private void TriggerOnGameStartedRpc()
     {
-            OnGameStarted?.Invoke(this, EventArgs.Empty);
+        OnGameStarted?.Invoke(this, EventArgs.Empty);
     }
 
     [Rpc(SendTo.Server)]
     public void ClickedOnGridPositionRpc(int x, int y, PlayerType playerType)
     {
-        if(playerType != currentPlayablePlayerType.Value)
+        if (playerType != currentPlayablePlayerType.Value)
         {
             return;
         }
-        OnClickedOnGridPosition?.Invoke(this, new OnClickedOnGridPositionEventArgs{
+
+        if (playerTypeArray[x, y] != PlayerType.None)
+        {
+            //Already Occupied
+            return;
+        }
+        playerTypeArray[x, y] = playerType;
+        OnClickedOnGridPosition?.Invoke(this, new OnClickedOnGridPositionEventArgs
+        {
             x = x,
             y = y,
             playerType = playerType
         });
 
-        switch(currentPlayablePlayerType.Value)
+        switch (currentPlayablePlayerType.Value)
         {
             default:
             case PlayerType.Cross:
@@ -92,6 +173,8 @@ public class GameManager : NetworkBehaviour
                 break;
         }
         // TriggerOnCurrentPlayablePlayerTypeChangedRpc();
+
+        TestWinner();
     }
 
     // [Rpc(SendTo.ClientsAndHost)]
@@ -100,6 +183,46 @@ public class GameManager : NetworkBehaviour
     //     OnCurrentPlayablePlayerTypeChanged?.Invoke(this, EventArgs.Empty);
     // }
 
+    private bool TestWinnerLine(Line line)
+    {
+        return TestWinnerLine(
+            playerTypeArray[line.gridVector2IntList[0].x, line.gridVector2IntList[0].y],
+            playerTypeArray[line.gridVector2IntList[1].x, line.gridVector2IntList[1].y],
+            playerTypeArray[line.gridVector2IntList[2].x, line.gridVector2IntList[2].y]
+        );
+    }
+    private bool TestWinnerLine(PlayerType aPlayerType, PlayerType bPlayerType, PlayerType cPlayerType)
+    {
+        return
+            aPlayerType != PlayerType.None &&
+            aPlayerType == bPlayerType &&
+            bPlayerType == cPlayerType;
+    }
+    private void TestWinner()
+    {
+        for (int i = 0; i < lineList.Count; i++)
+        {
+            Line line = lineList[i];
+            if (TestWinnerLine(line))
+            {
+                Debug.Log("Win!");
+                currentPlayablePlayerType.Value = PlayerType.None;
+                TriggerOnGameWinRpc(i, playerTypeArray[line.centerGridPosition.x, line.centerGridPosition.y]);
+                break;
+            }
+        }
+
+    }
+    [Rpc(SendTo.ClientsAndHost)]
+    private void TriggerOnGameWinRpc(int lineIndex, PlayerType winPlayerType)
+    {
+        Line line = lineList[lineIndex];
+        OnGameWin?.Invoke(this, new OnGameWinEventArgs
+        {
+            line = line,
+            winPlayerType = winPlayerType
+        });
+    }
     public PlayerType GetLocalPlayerType()
     {
         return localPlayerType;
--- Assets/Scripts/GameVisualManager.cs
+++ Assets/Scripts/GameVisualManager.cs
@@ -1,3 +1,4 @@
+using System;
 using Unity.Mathematics;
 using Unity.Netcode;
 using UnityEngine;
@@ -7,9 +8,33 @@ public class GameVisualManager : NetworkBehaviour
     private const float GRID_SIZE = 3.1f;
     [SerializeField] private Transform crossPrefab;
     [SerializeField] private Transform circlePrefab;
+    [SerializeField] private Transform lineCompletePrefab;
     void Start()
     {
         GameManager.Instance.OnClickedOnGridPosition += GameManager_OnClickedOnGridPosition;
+        GameManager.Instance.OnGameWin += GameManager_OnGameWin;
+    }
+
+    private void GameManager_OnGameWin(object sender, GameManager.OnGameWinEventArgs e)
+    {
+        if(!NetworkManager.Singleton.IsServer)
+        {
+            return;
+        }
+        float eulerZ = 0f;
+        switch(e.line.orientation)
+        {
+            default:
+            case GameManager.Orientation.Horizontal: eulerZ = 0f; break;
+            case GameManager.Orientation.Vertical: eulerZ = 90f; break;
+            case GameManager.Orientation.DiagonalA: eulerZ = 45f; break;
+            case GameManager.Orientation.DiagonalB: eulerZ = -45f; break;
+
+
+        }
+        Transform lineCompleteTransfrom =
+         Instantiate(lineCompletePrefab, GetGridWorldPosition(e.line.centerGridPosition.x, e.line.centerGridPosition.y),Quaternion.Euler(0,0,eulerZ));
+        lineCompleteTransfrom.GetComponent<NetworkObject>().Spawn(true);
     }
 
     private void GameManager_OnClickedOnGridPosition(object sender, GameManager.OnClickedOnGridPositionEventArgs e){
